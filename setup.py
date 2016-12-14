7fc713f30ff5282bbcdc02671359658b9e14be94
nathaniel smith <nathanielksmith@gmail.com>
2016-12-13 20:32:21 -0800

    add setup file

--- setup.py
+++ setup.py
@@ -1,2 +1,21 @@
-# TODO
-pass
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='tildetown-admin',
+    version='1.0.0',
+    description='administrative webapp for tilde.town',
+    url='https://github.com/nathanielksmith/prosaic',
+    author='vilmibm shaksfrpease',
+    author_email='vilmibm@protonmail.ch',
+    license='AGPL',
+    classifiers=[
+        'Topic :: Artistic Software',
+        'License :: OSI Approved :: Affero GNU General Public License v3 (AGPLv3)',
+    ],
+    packages=['ttadmin'],
+    install_requires = ['Django==1.10.2',
+                        'sshpubkeys==2.2.0',],
+    include_package_data = True,
+)
