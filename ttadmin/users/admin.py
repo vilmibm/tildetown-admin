620a91a65773e0d848aa61223196d14354bfd082
nathaniel smith <nathanielksmith@gmail.com>
2017-01-13 22:22:28 -0800

    hide vanilla auth stuff

--- ttadmin/users/admin.py
+++ ttadmin/users/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.models import Group
+
 from .models import Townie
+
+admin.site.unregister(User)
+admin.site.unregister(Group)
 
 @admin.register(Townie)
 class TownieAdmin(admin.ModelAdmin):
