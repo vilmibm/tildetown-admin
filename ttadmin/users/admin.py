50a9ed6298751b8d9295437d334150ae6a667384
nathaniel smith <nathanielksmith@gmail.com>
2017-01-13 22:34:08 -0800

    enable pubkey inline

--- ttadmin/users/admin.py
+++ ttadmin/users/admin.py
@@ -2,11 +2,15 @@ from django.contrib import admin
 from django.contrib.auth.models import User
 from django.contrib.auth.models import Group
 
-from .models import Townie
+from .models import Townie, Pubkey
 
 admin.site.unregister(User)
 admin.site.unregister(Group)
 
+class PubkeyInline(admin.TabularInline):
+    model = Pubkey
+
 @admin.register(Townie)
 class TownieAdmin(admin.ModelAdmin):
+    inlines = [PubkeyInline]
     pass
